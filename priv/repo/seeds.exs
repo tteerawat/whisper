57cf9b4e9379f46619b01627b2e393e9ae451726
teerawat1992 <teerawat.lamanchart@gmail.com>
2016-01-10 10:25:54 +0700

    Create a seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,11 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Whisper.{Repo, Post}
+
+Repo.delete_all(Post)
+
+Repo.insert!(%Post{title: "Awesome Ruby", url: "https://github.com/markets/awesome-ruby", favorite: true})
+Repo.insert!(%Post{title: "Awesome Elixir", url: "https://github.com/h4cc/awesome-elixir", favorite: true})
+Repo.insert!(%Post{title: "Awesome Rust", url: "https://github.com/kud1ing/awesome-rust"})
