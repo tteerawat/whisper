f866d705f45497f527e4ec7061cb2f0e35ca2a24
teerawat1992 <teerawat.lamanchart@gmail.com>
2016-09-24 11:41:18 +0700

    Update packages

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule Whisper.Mixfile do
       {:phoenix, "~> 1.2.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.0"},
-      {:postgrex, "~> 0.11.0"},
+      {:postgrex, "~> 0.12"},
       {:phoenix_html, "~> 2.3"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.9"},
